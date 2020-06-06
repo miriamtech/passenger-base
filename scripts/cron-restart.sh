dec756ca08bea087fe4e3ea32f658e430d5d9e30
Ken Treis <ken@miriamtech.com>
2020-06-06 12:24:10 -0700

    Add a random sleep before restarting nginx
    This helps prevent all of our containers from going down at the same
    time.

--- scripts/cron-restart.sh
+++ scripts/cron-restart.sh
@@ -3,6 +3,7 @@ TRIGGER=/var/run/reboot-required
 PACKAGES=/var/run/reboot-required.pkgs
 
 if [ -e $TRIGGER ]; then
+  sleep $[RANDOM%30]m;
   echo "Restarting nginx due to upgraded packages:"
   cat $PACKAGES
   service nginx stop # runit will restart
